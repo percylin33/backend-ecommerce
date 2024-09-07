61747534e6a0eeca75d4e6a9d0c66750dad398b4
percy <p3r.valderrama@gmail.com>
2024-09-07 02:42:32 -0500

    feat-cloudinari

--- config/plugins.js
+++ config/plugins.js
@@ -1,12 +1,18 @@
-// module.exports = {
-    
-//     'strapi-neon-tech-db-branches': {
-//       enabled: true,
-//       config: {
-//         neonApiKey: "ndze3qlwd1rn3h4v6wtb7yu3humca3u31bw0iakgqw0xij89g5saz10i2h26ymvw", // get it from here: https://console.neon.tech/app/settings/api-keys
-//         neonProjectName: "ecommers", // the neon project under wich your DB runs
-//         neonRole: "neondb_owner", // create it manually under roles for your project first
-//         gitBranch: "main", // branch can be pinned via this config option. Will not use branch from git then. Usefull for preview/production deployment
-//       }
-//     }
-//   }
+module.exports = ({ env }) => ({
+  
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+  
+});
